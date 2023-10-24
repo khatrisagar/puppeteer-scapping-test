78af4de5cdc2ad1a22052c1fd3b251d9362b6cab
khatrisagar <khatrisagar404@gmail.com>
2023-10-24 18:09:38 +0530

    added cheerio

---
+++ pages/api/cheerio.ts
@@ -0,0 +1,45 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+/* eslint-disable */
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const response = await axios.get(
+      "https://www.amazon.in/OnePlus-Wireless-Earbuds-Drivers-Playback/dp/B0C8JB3G5W/ref=lp_80662755031_1_1?sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&th=1"
+    );
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const productTitleElement = $("#productTitle");
+    const currentPriceElement = $(".priceToPay span.a-price-whole");
+    const originalPriceElement = $(".a-price.a-text-price span.a-offscreen");
+    const ratingElement = $(
+      "#acrPopover .a-declarative .a-popover-trigger .a-size-base"
+    );
+    const outOfStockElement = $("#availability span");
+
+    const productTitle = productTitleElement.text().trim();
+    const currentPrice = currentPriceElement.text().trim();
+    const originalPrice = originalPriceElement.text().trim();
+    const rating = ratingElement.text().trim();
+    const outOfStock =
+      outOfStockElement.text().trim().toLowerCase() === "currently unavailable";
+
+    const data = {
+      productTitle: productTitle,
+      currentPrice: currentPrice,
+      originalPrice: originalPrice,
+      rating: rating,
+      outOfStock: outOfStock,
+    };
+
+    console.log(data);
+
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching the web page:", error);
+  }
+};
+
+/* eslint-enable */
