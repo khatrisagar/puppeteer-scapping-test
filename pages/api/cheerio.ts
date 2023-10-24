578c604c3791ffcf1a7dfa1adb02beff4b3c4f40
khatrisagar <khatrisagar404@gmail.com>
2023-10-24 20:13:40 +0530

    testing for 50k request

--- pages/api/cheerio.ts
+++ pages/api/cheerio.ts
@@ -18,24 +18,61 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     );
     const outOfStockElement = $("#availability span");
 
-    const productTitle = productTitleElement.text().trim();
-    const currentPrice = currentPriceElement.text().trim();
-    const originalPrice = originalPriceElement.text().trim();
+    const productTitle = productTitleElement.text()?.trim();
+    const currentPrice = currentPriceElement.text()?.trim();
+    const originalPrice = originalPriceElement.text()?.trim();
 
     console.log("ratingElement");
-    const rating = ratingElement.text().trim().split(" ")?.[0];
+    const rating = ratingElement.text()?.trim()?.split(" ")?.[0];
     const outOfStock =
-      outOfStockElement.text().trim().toLowerCase() === "currently unavailable";
+      outOfStockElement.text()?.trim()?.toLowerCase() ===
+      "currently unavailable";
 
     const data = {
-      productTitle: productTitle,
-      currentPrice: currentPrice,
-      originalPrice: originalPrice,
-      rating: rating,
-      outOfStock: outOfStock,
+      productTitle,
+      currentPrice,
+      originalPrice,
+      rating,
+      outOfStock,
     };
 
     console.log(data);
+
+    for (let i = 0; i < 50000; i++) {
+      const response = await axios.get(
+        "https://www.amazon.in/OnePlus-Wireless-Earbuds-Drivers-Playback/dp/B0C8JB3G5W/ref=lp_80662755031_1_1?sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&th=1"
+      );
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      const productTitleElement = $("#productTitle");
+      const currentPriceElement = $(".priceToPay span.a-price-whole");
+      const originalPriceElement = $(".a-price.a-text-price span.a-offscreen");
+      const ratingElement = $(
+        "#acrPopover .a-declarative .a-popover-trigger .a-color-base"
+      );
+      const outOfStockElement = $("#availability span");
+
+      const productTitle = productTitleElement.text()?.trim();
+      const currentPrice = currentPriceElement.text()?.trim();
+      const originalPrice = originalPriceElement.text()?.trim();
+
+      console.log("ratingElement");
+      const rating = ratingElement.text()?.trim()?.split(" ")?.[0];
+      const outOfStock =
+        outOfStockElement.text()?.trim()?.toLowerCase() ===
+        "currently unavailable";
+
+      const data = {
+        productTitle,
+        currentPrice,
+        originalPrice,
+        rating,
+        outOfStock,
+      };
+
+      console.log("data", i, data);
+    }
 
     res.json(data);
   } catch (error) {
